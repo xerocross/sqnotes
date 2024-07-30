5ca9f2c1157ddf92d09292757649a57d386b451d
Adam <adamfgcross@gmail.com>
2024-07-29 21:42:06 -0400

    add some testing for configuration module

---
+++ test/test_configuration_module/test_is_config_file_exists.py
@@ -0,0 +1,23 @@
+
+import pytest
+from unittest.mock import patch
+from test.test_configuration_module.test_helper import configuration_module, test_temporary_directory
+from sqnotes.configuration_module import ConfigurationModule
+
+def describe_configuration_module():
+    
+    def describe_is_config_file_exists():
+        
+        def describe_config_file_does_exist():
+            
+            @patch('os.path.exists', lambda x : True)
+            def it_returns_true(configuration_module : ConfigurationModule):
+                is_config_file_exists = configuration_module.is_config_file_exists()
+                assert is_config_file_exists == True
+                
+        def describe_config_file_does_not_exist():
+            
+            @patch('os.path.exists', lambda x : False)
+            def it_returns_false(configuration_module : ConfigurationModule):
+                is_config_file_exists = configuration_module.is_config_file_exists()
+                assert is_config_file_exists == False
---
+++ test/test_configuration_module/test_setting_and_getting_config.py
@@ -0,0 +1,59 @@
+
+from unittest.mock import patch
+import os
+import pytest
+from sqnotes.configuration_module import ConfigurationModule
+import logging
+
+from test.test_configuration_module.test_helper import configuration_module, test_temporary_directory
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+
+@pytest.fixture(scope='session', autouse=True)
+def set_test_environment():
+    os.environ['TESTING'] = 'true'
+    
+
+def describe_the_configuration_module():
+
+    def it_sets_and_gets_config_user_settings(
+                                    configuration_module : ConfigurationModule
+                                    ):
+        configuration_module.open_or_create_and_open_user_config_file()
+        key = 'my_key'
+        value = 'apple'
+        configuration_module.set_setting_to_user_config(key=key, value=value)
+        assert configuration_module.get_setting_from_user_config(key = key) == value
+        
+        
+    def it_sets_and_gets_config_global_settings(
+                                    configuration_module : ConfigurationModule
+                                    ):
+        configuration_module.open_or_create_and_open_user_config_file()
+        key = 'my_key'
+        value = 'apple'
+        configuration_module.set_global_to_user_config(key=key, value=value)
+        assert configuration_module.get_global_from_user_config(key = key) == value
+        
+    def it_returns_null_on_get_unknown_setting_key(
+                                            configuration_module : ConfigurationModule
+                                                ):
+        configuration_module.open_or_create_and_open_user_config_file()
+        value = configuration_module.get_setting_from_user_config(key='unknown')
+        assert value == None
+        
+        
+    def it_returns_null_on_get_unknown_global_key(
+                                            configuration_module : ConfigurationModule
+                                                ):
+        configuration_module.open_or_create_and_open_user_config_file()
+        value = configuration_module.get_global_from_user_config(key='unknown')
+        assert value == None
+        
+    
+
+    
+    
