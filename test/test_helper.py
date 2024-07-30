ef26931195834537883bcfc0de9401e9610eb273
Adam <adamfgcross@gmail.com>
2024-07-29 20:13:55 -0400

    add some test helpers

--- test/test_helper.py
+++ test/test_helper.py
@@ -9,3 +9,9 @@ def get_all_single_arg_inputs(mocked_fn):
     call_args = mocked_fn.call_args_list
     arguments_list = [args[0] for args, _ in call_args]
     return arguments_list
+
+def do_nothing(*args, **kwargs):
+    pass
+
+def get_true(*args, **kwargs):
+    return True
