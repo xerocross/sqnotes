f6552b8e7c393d2ea533376b7c7eafa97947ba28
Adam <adamfgcross@gmail.com>
2024-07-31 20:49:52 -0400

    add support for max attempts on user input

--- src/sqnotes/user_input_helper.py
+++ src/sqnotes/user_input_helper.py
@@ -5,15 +5,26 @@ VALIDATION_FAILURE_MESSAGE = 'Input {} failed validation.'
 class ValidationFailureException(Exception):
     """Raise if input fails user-input validation function."""
 
+class ExceededMaxInputAttemptsException(Exception):
+    """Raise if exceeds max number of allowed input attempts."""
+
 
 class UserInputHelper:
 
     @staticmethod
     def get_string_input(prompt, 
                          validator = None, 
-                         validation_failure_message = None):
+                         validation_failure_message = None,
+                         attempt_limit = None):
         user_input = None
+        attempt_number = 0
         while True:
+            attempt_number += 1
+            if attempt_limit is not None:
+                if attempt_number > attempt_limit:
+                    raise ExceededMaxInputAttemptsException(f"Exceeded {attempt_limit} attempts.")
+            
+            
             print(prompt)
             user_input = input()
             if validator is not None:
--- test/test_user_input_helper/test_string_input.py
+++ test/test_user_input_helper/test_string_input.py
@@ -2,7 +2,8 @@
 import pytest
 from unittest.mock import patch, Mock
 from sqnotes.user_input_helper import UserInputHelper,\
-    ValidationFailureException, VALIDATION_FAILURE_MESSAGE
+    VALIDATION_FAILURE_MESSAGE,\
+    ExceededMaxInputAttemptsException
 from test.test_helper import get_all_mocked_print_output,\
     get_all_mocked_print_output_to_string
 
@@ -13,19 +14,37 @@ def user_input_helper() -> UserInputHelper:
     user_input_helper = UserInputHelper()
     yield user_input_helper
     
+@pytest.fixture
+def mock_input():
+    with patch('builtins.input') as mock:
+        yield mock
+        
+@pytest.fixture
+def mock_print():
+    with patch('builtins.print') as mock:
+        yield mock
 
+@pytest.fixture
+def mock_validator():
+    validator = Mock()
+    yield validator
 
     
+test_prompt = "Specify some string.> "
+second_user_input = "second input"
+test_user_input = 'apple'
+test_third_input = 'pear'
+test_second_user_input = 'pear'
+validation_failure = 'validation failure message'    
+
 def describe_user_input_helper():
     
-    @patch('builtins.print')
-    @patch('builtins.input')
+
     def it_prompts_user_with_specified_prompt(
                                             mock_input,
                                             mock_print,
                                             user_input_helper
                                                 ):
-        test_prompt = "test get input"
         test_user_input = 'apple'
         mock_input.side_effect = [test_user_input]
         user_input_helper.get_string_input(prompt = test_prompt)
@@ -33,15 +52,12 @@ def describe_user_input_helper():
         assert test_prompt in output
         
         
-    
-    @patch('builtins.print')
-    @patch('builtins.input')
+
     def it_passes_input_through_validator (
                                             mock_input,
                                             mock_print,
                                             user_input_helper
                                                 ):
-        test_prompt = "test get input"
         test_user_input = 'apple'
         validator = Mock()
         validator.side_effect = [True, True]
@@ -50,19 +66,35 @@ def describe_user_input_helper():
         validator.assert_called_once_with(test_user_input)
         
         
+    def describe_validation_passes():
+        
+        def it_returns_input(
+                                mock_input,
+                                mock_print,
+                                user_input_helper
+                                ):
+            test_user_input = 'apple'
+            validation_failure = 'validation failure message'
+            validator = Mock()
+            validator.side_effect = [True, True]
+            mock_input.side_effect = [test_user_input, second_user_input]
+            result = user_input_helper.get_string_input(prompt = test_prompt, 
+                                                   validator = validator,
+                                                   validation_failure_message = validation_failure
+                                                   )
+            assert result == test_user_input
+        
+        
     def describe_validation_fails():
         
         
         def describe_validation_failure_message_specified():
         
-            @patch('builtins.print')
-            @patch('builtins.input')
             def it_prints_validation_failure_message (
                                                     mock_input,
                                                     mock_print,
                                                     user_input_helper
                                                         ):
-                test_prompt = "test get input"
                 test_user_input = 'apple'
                 validation_failure = 'validation failure message'
                 validator = Mock()
@@ -75,14 +107,11 @@ def describe_user_input_helper():
                 output = get_all_mocked_print_output_to_string(mocked_print=mock_print)
                 assert validation_failure in output       
             
-            @patch('builtins.print')
-            @patch('builtins.input')
             def it_prompts_for_input_again (
                                                     mock_input,
                                                     mock_print,
                                                     user_input_helper
                                                         ):
-                test_prompt = "test get input"
                 test_user_input = 'apple'
                 validation_failure = 'validation failure message'
                 validator = Mock()
@@ -97,16 +126,33 @@ def describe_user_input_helper():
                 assert num_prompts == 2 
             
             
+            def describe_second_attempt_passes_validation():
+                
+                
+                def it_returns_second_input(
+                                                    mock_input,
+                                                    mock_print,
+                                                    user_input_helper
+                                                    ):
+                    test_user_input = 'apple'
+                    validation_failure = 'validation failure message'
+                    validator = Mock()
+                    validator.side_effect = [False, True]
+                    mock_input.side_effect = [test_user_input, second_user_input]
+                    result = user_input_helper.get_string_input(prompt = test_prompt, 
+                                                           validator = validator,
+                                                           validation_failure_message = validation_failure
+                                                           )
+                    assert result == second_user_input
+                    
+            
         def describe_validation_failure_not_specified():
         
-            @patch('builtins.print')
-            @patch('builtins.input')
             def it_prints_validation_failure_message (
                                                     mock_input,
                                                     mock_print,
                                                     user_input_helper
                                                         ):
-                test_prompt = "test get input"
                 test_user_input = 'apple'
                 validator = Mock()
                 validator.side_effect = [False, True]
@@ -118,5 +164,129 @@ def describe_user_input_helper():
                 assert VALIDATION_FAILURE_MESSAGE in output       
             
         
+        def describe_limit_attempts_is_2():
+            
+            attempt_limit = 2
+            
+            def describe_validation_passes_first_attmept():
+                
+                def it_returns_input(
+                                        mock_input,
+                                        mock_print,
+                                        user_input_helper,
+                                        attempt_limit = attempt_limit
+                                        ):
+                    
+                    validation_failure = 'validation failure message'
+                    validator = Mock()
+                    validator.side_effect = [True, True]
+                    mock_input.side_effect = [test_user_input, second_user_input]
+                    result = user_input_helper.get_string_input(prompt = test_prompt, 
+                                                           validator = validator,
+                                                           validation_failure_message = validation_failure,
+                                                           attempt_limit = attempt_limit
+                                                           )
+                    assert result == test_user_input
+            
+            
+            def describe_validation_fails():
+            
+            
+                def describe_second_input_valid():
+                
+                    def it_prompts_for_input_again (
+                                                                mock_input,
+                                                                mock_print,
+                                                                user_input_helper,
+                                                                mock_validator
+                                                                    ):
+                        mock_validator.side_effect = [False, True]
+                        mock_input.side_effect = [test_user_input, test_second_user_input]
+                        user_input_helper.get_string_input(prompt = test_prompt, 
+                                                               validator = mock_validator,
+                                                               validation_failure_message = validation_failure,
+                                                               attempt_limit = attempt_limit
+                                                               )
+                        output = get_all_mocked_print_output_to_string(mocked_print=mock_print)
+                        num_prompts = output.count(test_prompt)
+                        assert num_prompts == 2 
+            
+            
+            
+            
+                    def it_returns_second_input(
+                                                    mock_input,
+                                                    mock_print,
+                                                    user_input_helper,
+                                                    mock_validator
+                                                    ):
+                        mock_validator.side_effect = [False, True]
+                        mock_input.side_effect = [test_user_input, second_user_input]
+                        result = user_input_helper.get_string_input(prompt = test_prompt, 
+                                                               validator = mock_validator,
+                                                               validation_failure_message = validation_failure,
+                                                               attempt_limit = attempt_limit
+                                                               )
+                        assert result == second_user_input
+            
+                def describe_second_input_fails_validation():
+                    
+                    
+                    def it_raises_ExceededMaxInputAttemptsException (
+                                                                mock_input,
+                                                                mock_print,
+                                                                user_input_helper,
+                                                                mock_validator
+                                                                    ):
+                        mock_validator.side_effect = [False, False]
+                        mock_input.side_effect = [test_user_input, test_second_user_input]
+                        with pytest.raises(ExceededMaxInputAttemptsException):
+                            user_input_helper.get_string_input(prompt = test_prompt, 
+                                                               validator = mock_validator,
+                                                               validation_failure_message = validation_failure,
+                                                               attempt_limit = attempt_limit
+                                                               )
+                        
+        def describe_attempt_limit_is_3():
+                      
+                    
+            attempt_limit = 3
+            
+            def describe_first_two_attempts_fail():
+                
+                def describe_third_attempt_fails():
+            
+                    def it_raises_ExceededMaxInputAttemptsException (
+                                                                mock_input,
+                                                                mock_print,
+                                                                user_input_helper,
+                                                                mock_validator
+                                                                ):
+                        mock_validator.side_effect = [False, False, False]
+                        mock_input.side_effect = [test_user_input, test_second_user_input, test_third_input]
+                        with pytest.raises(ExceededMaxInputAttemptsException):
+                            user_input_helper.get_string_input(prompt = test_prompt, 
+                                                               validator = mock_validator,
+                                                               validation_failure_message = validation_failure,
+                                                               attempt_limit = attempt_limit
+                                                               )
+                        
         
+                def describe_third_attempt_passes():
+                    
+                    def it_returns_third_input(
+                                                    mock_input,
+                                                    mock_print,
+                                                    user_input_helper,
+                                                    mock_validator
+                                                    ):
+                        mock_validator.side_effect = [False, False, True]
+                        mock_input.side_effect = [test_user_input, second_user_input, test_third_input]
+                        result = user_input_helper.get_string_input(prompt = test_prompt, 
+                                                               validator = mock_validator,
+                                                               validation_failure_message = validation_failure,
+                                                               attempt_limit = attempt_limit
+                                                               )
+                        assert result == test_third_input
+                    
         
