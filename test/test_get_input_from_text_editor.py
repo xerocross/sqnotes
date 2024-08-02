c0a4c3df21c2a20fa2e3c5f2b977f6a73a006905
Adam <adamfgcross@gmail.com>
2024-08-02 15:09:34 -0400

    rewrite tests in pytest idioms from unittest

--- test/test_get_input_from_text_editor.py
+++ test/test_get_input_from_text_editor.py
@@ -1,113 +1,75 @@
 
-import unittest
-from unittest.mock import patch, mock_open, MagicMock
-import os
+from unittest.mock import patch, mock_open
 import pytest
 from sqnotes.sqnotes_module import SQNotes
-import tempfile
-from injector import Injector
 from sqnotes.custom_exceptions import TextEditorSubprocessException
 
-@pytest.fixture(scope='session', autouse=True)
-def set_test_environment():
-    os.environ['TESTING'] = 'true'
+
+TEXT_EDITOR = 'vim'
+MOCK_NOTE_CONTENT = 'mock content'
+
+
+def describe_get_input_from_text_editor():
     
+    @pytest.mark.usefixtures('mock_NamedTemporaryFile')
+    def it_calls_subprocess_with_correct_temp_filename (
+                                                        sqnotes_obj :SQNotes, 
+                                                        mock_subprocess_call,
+                                                        mock_temp_file
+                                                        ):
+        
     
-def delete_temp_file(*args, **kwargs):
-    pass
-        
-class TestGetInputFromTextEditor(unittest.TestCase):
-    delete_temp_file_patcher = None
-    
-    @classmethod
-    def setUpClass(cls):
-        cls.delete_temp_file_patcher = patch.object(SQNotes, "_delete_temp_file", delete_temp_file)
-        cls.delete_temp_file_patcher.start()
-
-        
-    @classmethod
-    def tearDownClass(cls):
-        cls.delete_temp_file_patcher.stop()
-
-
-
-    def setUp(self):
-        injector = Injector()
-        self.sqnotes = injector.get(SQNotes)
-        self.TEXT_EDITOR = 'vim'
-        mock_temp_file = MagicMock(spec=tempfile.NamedTemporaryFile)
-        mock_temp_file.name = "mock_temp_file_name"
-        self.mock_temp_file = mock_temp_file
-        
-        mock_tempfile = MagicMock(spec=tempfile.NamedTemporaryFile)
-        mock_tempfile.return_value.__enter__.return_value = self.mock_temp_file
-        
-        self.mock_tempfile_patcher = patch('tempfile.NamedTemporaryFile', mock_tempfile)
-        self.mock_tempfile_patcher.start()
-        self.mock_note_content = 'Mock content'
-        mock_open_function = mock_open(read_data=self.mock_note_content)
-        
-        self.mock_open_patcher = patch('builtins.open', mock_open_function)
-        self.mock_open_patcher.start()
-        
-        
-        
-    def tearDown(self):
-        self.mock_tempfile_patcher.stop()
-        self.mock_open_patcher.stop()
-        
-
-    @patch('subprocess.call')
-    def test_input_function_calls_subprocess_with_correct_temp_filename(self, 
-                                                                        mock_subprocess_call):
-        
-
-        # Mock subprocess.call to simulate editing
         mock_subprocess_call.return_value = 0  # Simulate successful call
-
-        self.sqnotes._get_input_from_text_editor(TEXT_EDITOR=self.TEXT_EDITOR)
-
+        sqnotes_obj._get_input_from_text_editor(TEXT_EDITOR=TEXT_EDITOR)
         mock_subprocess_call.assert_called_once()
-        called_args, _ = mock_subprocess_call.call_args
-        first_called_args = called_args[0]
-        self.assertEqual(first_called_args[1], self.mock_temp_file.name)
-
+        call_args_list = mock_subprocess_call.call_args_list
+        first_called_args, _ = call_args_list[0]
+        passed_gpg_command = first_called_args[0]
+        argument_in_filename_position = passed_gpg_command[1]
+        assert argument_in_filename_position == mock_temp_file.name
         
-    @patch('tempfile.NamedTemporaryFile')
-    @patch('subprocess.call')
-    def test_returns_content_from_text_editor(self, 
-                                              mock_subprocess_call, 
-                                              mock_tempfile):
         
+    @pytest.mark.usefixtures('mock_NamedTemporaryFile',
+                             'mock_delete_temp_file')
+    def it_returns_content_from_text_editor(
+                                                sqnotes_obj :SQNotes,
+                                                mock_subprocess_call
+                                            ):
 
-        # Mock subprocess.call to simulate editing
-        mock_subprocess_call.return_value = 0  # Simulate successful call
-        note_content = self.sqnotes._get_input_from_text_editor(TEXT_EDITOR=self.TEXT_EDITOR)
-
-        self.assertEqual(note_content, self.mock_note_content)    
-    
-    
-    @patch('tempfile.NamedTemporaryFile')
-    @patch('subprocess.call')
-    def test_get_input_raises_exception_on_failing_subprocess_with_exception(self, 
-                                                                             mock_subprocess_call, 
-                                                                             mock_tempfile):
+        mock_open_function = mock_open(read_data = MOCK_NOTE_CONTENT)
+        with patch('builtins.open', mock_open_function):
+            mock_subprocess_call.return_value = 0  # Simulate successful call
+            note_content = sqnotes_obj._get_input_from_text_editor(TEXT_EDITOR=TEXT_EDITOR)
+            print(note_content)
+            assert note_content == MOCK_NOTE_CONTENT
         
-
-        mock_subprocess_call.side_effect = Exception()
-
-        with self.assertRaises(TextEditorSubprocessException):
-            self.sqnotes._get_input_from_text_editor(TEXT_EDITOR = self.TEXT_EDITOR)
+        
+    def describe_gpg_subprocess_raises_exception():
+        
+        @pytest.mark.usefixtures('mock_NamedTemporaryFile',
+                                 'mock_delete_temp_file')
+        def it_raises_exception (
+                                sqnotes_obj,
+                                mock_subprocess_call, 
+                             ):
     
-    @patch('tempfile.NamedTemporaryFile')
-    @patch('subprocess.call')
-    def test_get_input_raises_exception_on_failing_subprocess_with_nonzero_response(self, 
-                                                                                    mock_subprocess_call, 
-                                                                                    mock_tempfile):
-
-        mock_subprocess_call.return_value = 1
-        with self.assertRaises(TextEditorSubprocessException):
-            self.sqnotes._get_input_from_text_editor(TEXT_EDITOR=self.TEXT_EDITOR)
     
+            mock_subprocess_call.side_effect = Exception()
+            with pytest.raises(TextEditorSubprocessException):
+                sqnotes_obj._get_input_from_text_editor(TEXT_EDITOR = TEXT_EDITOR)
+        
+    def describe_gpg_returns_error_code():
+        
+        @pytest.mark.usefixtures('mock_NamedTemporaryFile',
+                                 'mock_delete_temp_file')
+        def it_raises_exception(
+                                sqnotes_obj,
+                                mock_subprocess_call, 
+                            ):
+    
+            mock_subprocess_call.return_value = 1
+            with pytest.raises(TextEditorSubprocessException):
+                sqnotes_obj._get_input_from_text_editor(TEXT_EDITOR=TEXT_EDITOR)
+
         
         
