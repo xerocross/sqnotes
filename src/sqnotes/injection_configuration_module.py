889b7ea7040ee9fc11f7c4f4cf77a81cf87bfebd
Adam <adamfgcross@gmail.com>
2024-08-03 12:49:39 -0400

    add configuration to inject config file path into SQNotesConfig

--- src/sqnotes/injection_configuration_module.py
+++ src/sqnotes/injection_configuration_module.py
@@ -1,13 +1,20 @@
 
-from injector import Module, inject, Injector, singleton
+from injector import Module, inject, Injector, singleton, provider
 from sqnotes.sqnotes_logger import SQNotesLogger
 from sqnotes.user_configuration_helper import UserConfigurationHelper
 import os
-from dotenv import load_dotenv
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sqnotes_dir = os.path.abspath(os.path.join(current_dir, "../"))
+config_file_path = os.path.join(sqnotes_dir, 'sqnotes/config.yaml')
 
 
 class InjectionConfigurationModule(Module):
+    
+    @provider
+    def provide_config_file_path(self) -> str:
+        return config_file_path
+    
     def configure(self, binder):
         binder.bind(SQNotesLogger, to=SQNotesLogger(), scope=singleton)
         binder.bind(UserConfigurationHelper, to=UserConfigurationHelper(), scope=singleton)
